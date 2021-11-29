5d700dbd1fa78f21c353dea7130b3d9c5873eff8
Анастасия <Nasty-a.a@yandex.ru>
2021-11-29 18:52:50 +0300

    Задание 1,2

--- app1/src/components/Message.js
+++ app1/src/components/Message.js
@@ -1,9 +1,41 @@
+import {useState} from 'react';
+
+
 function Message(props) {
+
+    const [MessageList, setMessageList] = useState([]);
+    const [value, setValue] = useState('');
+
+    const onChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+            const copyMessageList = [...MessageList];
+            copyMessageList.push({
+                author: 'Автор',
+                text:value,
+            })
+            setMessageList(copyMessageList);
+    }
+
     return (
       <div className="App">
-        <p className="p_text">Текст из константы - 
+        <p className="p_text">
           <h1 className="h1_text">{props.text}</h1>
         </p>
+        <div>
+        <div className="MesList">
+            {       
+                MessageList.map((item)=> <div>{item.text}</div>)
+            }
+        </div>
+        <form onSubmit={onSubmit} action="">
+            <input type='text' onChange={onChange} value={value}></input>
+            <button type="submit">Отправить</button>
+        </form>
+        </div>
       </div>
     );
    }
--- app1/src/index.js
+++ app1/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Message from './components/Message';
 
-
 ReactDOM.render(
  <React.StrictMode>
     <Message text={'Text for Message'} />
