bd61d99ccdf941f49ea11238e21843c9647b98f0
Анастасия <Nasty-a.a@yandex.ru>
2021-11-29 17:04:44 +0300

    less1

---
+++ app1/src/components/Message.js
@@ -0,0 +1,11 @@
+function Message(props) {
+    return (
+      <div className="App">
+        <p className="p_text">Текст из константы - 
+          <h1 className="h1_text">{props.text}</h1>
+        </p>
+      </div>
+    );
+   }
+  
+  export default Message;
