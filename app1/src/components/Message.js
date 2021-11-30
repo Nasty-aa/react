5615eaca445300781369dd777c4cd32f16fa1de6
Анастасия <Nasty-a.a@yandex.ru>
2021-11-30 22:30:02 +0300

    hw2

--- app1/src/components/Message.js
+++ app1/src/components/Message.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useEffect,useState} from 'react';
 
 
 function Message(props) {
@@ -11,27 +11,50 @@ function Message(props) {
     }
 
     const onSubmit = (event) => {
-        event.preventDefault();
+        if(value) {
+            event.preventDefault();
             const copyMessageList = [...MessageList];
             copyMessageList.push({
-                author: 'Автор',
+                author: 'user',
                 text:value,
             })
             setMessageList(copyMessageList);
+            setValue('');
+        }
     }
+
+    useEffect(()=>{
+
+        if(MessageList.length !== 0){
+            if(MessageList[MessageList.length-1].author === 'user'){
+            
+            const timerId = setTimeout(()=>{
+                const copyMessageList = [...MessageList];
+                copyMessageList.push({
+                    author: 'robot',
+                    text: 'Дождитесь ответа оператора',
+                })
+                setMessageList(copyMessageList);
+            },1500)
+
+            return () => {
+                clearTimeout(timerId);
+              }
+        }}
+    },[MessageList])
 
     return (
       <div className="App">
-        <p className="p_text">
+        <div className="p_text">
           <h1 className="h1_text">{props.text}</h1>
-        </p>
+        </div>
         <div>
         <div className="MesList">
             {       
-                MessageList.map((item)=> <div>{item.text}</div>)
+                MessageList.map((item)=> <div><div className={item.author}><p>{item.author === 'user' ? 'Вы':'Дружелюбный робот'}</p>{item.text}</div></div>)
             }
         </div>
-        <form onSubmit={onSubmit} action="" className="form">
+        <form onSubmit={onSubmit} action="#" className="form">
             <input className="input" type='text' onChange={onChange} value={value}></input>
             <button className="submit" type="submit">Отправить</button>
         </form>
