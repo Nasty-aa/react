23b31dc411c8165e2fe686ea490a9ec551731f90
Анастасия <Nasty-a.a@yandex.ru>
2021-12-12 21:13:09 +0300

    hw5

--- app1/src/App.js
+++ app1/src/App.js
@@ -10,33 +10,33 @@ import Chats from './components/Chats';
 function App(props) {
 
     return (
-        <div className="Container_App">
-            <BrowserRouter>
-                <header>
-                    <nav>
-                        <ul className="header_links">
-                            <li className="header_links_li"><Link to='/' className="header_links_a">Домашняя</Link></li>
-                            <li className="header_links_li"><Link to='/Profile' className="header_links_a">Моя страница</Link></li>
-                            <li className="header_links_li"><Link to='/Chat' className="header_links_a">Список чатов</Link></li>
-                        </ul>
-                    </nav>
-                </header>
-                <Switch>
-                    <Route exact path='/'>
-                        <Home />
-                    </Route>
-                    <Route path='/Profile'>
-                        <Profile />
-                    </Route>
-                    <Route path='/Chat'>
-                        <Chats />
-                    </Route>
-                    <Route>
-                        <h3 className="Page-not-found">Page not found 404 </h3>
-                    </Route>
-                </Switch>
-            </BrowserRouter>
-        </div>
+            <div className="Container_App">
+                <BrowserRouter>
+                    <header>
+                        <nav>
+                            <ul className="header_links">
+                                <li className="header_links_li"><Link to='/' className="header_links_a">Домашняя</Link></li>
+                                <li className="header_links_li"><Link to='/Profile' className="header_links_a">Моя страница</Link></li>
+                                <li className="header_links_li"><Link to='/Chat' className="header_links_a">Список чатов</Link></li>
+                            </ul>
+                        </nav>
+                    </header>
+                    <Switch>
+                        <Route exact path='/'>
+                            <Home />
+                        </Route>
+                        <Route path='/Profile'>
+                            <Profile />
+                        </Route>
+                        <Route path='/Chat'>
+                            <Chats />
+                        </Route>
+                        <Route>
+                            <h3 className="Page-not-found">Page not found 404 </h3>
+                        </Route>
+                    </Switch>
+                </BrowserRouter>
+            </div>
       );
 }
 
--- app1/src/components/Profile.js
+++ app1/src/components/Profile.js
@@ -1,10 +1,20 @@
 import {useState} from 'react';
+import {Checkbox} from '@mui/material';
+import {useDispatch, useSelector} from 'react-redux';
+import {profileCheckbox} from '../store/store';
 
 function Profile(props) {
-
+  const dispatch = useDispatch();
+  
+  
     return (
         <div className="Profile">
           <h1 className="Profile_h1">Profile</h1>
+          <Checkbox label="Задание 4" onClick={() => {
+              dispatch({
+                type: profileCheckbox,
+              })
+            }}/>
         </div>
       );
 }
